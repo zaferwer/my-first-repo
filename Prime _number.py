6f19827beea4f77d7011254c0a6ea63192abe534
zaferwer <67260479+zaferwer@users.noreply.github.com>
2020-07-10 02:11:47 +0300

    Add files via upload

---
+++ Prime _number.py
@@ -0,0 +1,13 @@
+number = int(input("Please enter the number you want to learn if it is a prime number:"))
+x = []
+#this part is for making the program faster. 
+if number%2==0 or number<0:
+    print(f"{number} is NOT a prime number")
+else:    
+    for i in range(2,number):
+        if number%i==0:
+            print(f"{number} is NOT a prime number")
+            break 
+        else:
+            print(f"{number} is a prime number")
+            break 
