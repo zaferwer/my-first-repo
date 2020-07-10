751f0282d965584a01dc6ba7f414865781e96efc
zaferwer <67260479+zaferwer@users.noreply.github.com>
2020-07-10 13:15:16 +0300

    Add files via upload

--- Prime _number.py
+++ Prime _number.py
@@ -4,9 +4,8 @@ if number%2==0 or number<0:
     print(f"{number} is NOT a prime number")
 else:    
     for i in range(2,number):
-        if number%i==0:
+        if number % i == 0 :
             print(f"{number} is NOT a prime number")
-            break 
-        else:
-            print(f"{number} is a prime number")
-            break 
+            break
+    else:
+        print(f"{number} is a prime number")
