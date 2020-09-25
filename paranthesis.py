d5ef1b754282d3f6df4d7f43b14ad8e66775bf59
zaferwer <zaferwer@gmail.com>
2020-09-26 01:44:11 +0300

    working

--- paranthesis.py
+++ paranthesis.py
@@ -1,16 +1,6 @@
-x = ""
-xlist=list(x)
-for i in ["()","{}","[]"]:
-    if i in x or len(x)==0:
-        result=True
-    else:
-        for i in range(len(xlist)//2):
-            if xlist[i]=="(" and xlist[-(i+1)]==")":
-                result=True
-            elif xlist[i]=="[" and xlist[-(i+1)]=="]":
-                result=True
-            elif xlist[i]=="{" and xlist[-(i+1)]=="}":
-                result=True
-            else:
-                result=False
-print(result)
+x = "(){((([])))}"
+for i in x:
+    x = x.replace("()","")
+    x = x.replace("[]","")
+    x = x.replace("{}","")
+print(not bool(x))
