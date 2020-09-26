8165669ad26ce55d287efb383d04863b8e668468
zaferwer <zaferwer@gmail.com>
2020-09-26 22:35:45 +0300

    last change

--- paranthesis.py
+++ paranthesis.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 x = "{[()[({})]]}"
 for i in range(len(x)//2):
     x = x.replace("()", "")
     x = x.replace("[]", "")
     x = x.replace("{}", "")
-=======
-x = "{[]}"
-for i in range(len(x)//2):
-    x = x.replace("()","")
-    x = x.replace("[]","")
-    x = x.replace("{}","")
->>>>>>> a08daceccaba833f5aa10f5808b7c0c0adb23f7c
 print(not bool(x))
