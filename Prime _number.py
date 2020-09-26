812789a6864ebd10bcf47765f7bc7729a2ed23d5
zaferwer <zaferwer@gmail.com>
2020-09-26 22:22:21 +0300

    27 september

--- Prime _number.py
+++ Prime _number.py
@@ -1,11 +1,11 @@
-number = int(input("Please enter the number you want to learn if it is a prime number:"))
-if number<=0:
+number = int(
+    input("Please enter the number you want to learn if it is a prime number:"))
+if number <= 0:
     print(f"{number} is NOT a prime number")
-else:    
-    for i in range(2,number):
-        if number%i==0:
+else:
+    for i in range(2, number):
+        if number % i == 0:
             print(f"{number} is NOT a prime number")
-            break 
+        break
     else:
-            print(f"{number} is a prime number")
-            
+        print(f"{number} is a prime number")
--- paranthesis.py
+++ paranthesis.py
@@ -1,6 +1,6 @@
-x = "{[]}"
+x = "{[()[({})]]}"
 for i in range(len(x)//2):
-    x = x.replace("()","")
-    x = x.replace("[]","")
-    x = x.replace("{}","")
+    x = x.replace("()", "")
+    x = x.replace("[]", "")
+    x = x.replace("{}", "")
 print(not bool(x))
